65dd63825de732e8333b256e5eac66892347d330
Eric Deng <ericg526652@gmail.com>
2016-03-03 20:22:54 +0800

    New Feature: get video description get video like and dislike number

--- hw_1/hw1-2.py
+++ hw_1/hw1-2.py
@@ -38,6 +38,7 @@ def get_description(results, number):
         i+=1
 
 if __name__ == '__main__':
+    '''parser'''
     argPrser = argparse.ArgumentParser(description='Process youtube search crawler argument.')
     argPrser.add_argument('-n', help="number of search result. default is 5", metavar="number", type=int, default=5)
     argPrser.add_argument('-p', help="page that you parse", metavar="page", type=int, default=1)
@@ -45,23 +46,48 @@ if __name__ == '__main__':
     args = argPrser.parse_args()
     # print args
 
-    # Process args
+    '''Process args'''
     term = args.keyword[0]
     # print term
     page = str(args.p)
     number = args.n
 
-    # get html
+    ''' get html '''
     url = "https://www.youtube.com/results?search_query=" + term + "&page=" + page
     html = requests.get(url)
     # print html.text
 
-    # BeautifulSoup
+    ''' BeautifulSoup'''
     soup = BeautifulSoup(html.text)
     # print soup.prettify()
     results = soup.find(id="results")
 
+    '''variable'''
+    youtubeUrl = "https://www.youtube.com"
+    i=0
 
-    get_description(results, number)
+    for content in results.find_all("div", "yt-lockup-content"):
+        title = content.h3.a.string
+        link = content.h3.a.get('href')
+        completeUrl = youtubeUrl+link
+        description = content.h3.next_sibling.next_sibling.next_sibling
+        print [i], title+"("+completeUrl+")"
+        if description != None:
+            print description.string
+        else:
+            print "None"
+        videoHtml = requests.get(completeUrl)
+        videoSoup = BeautifulSoup(videoHtml.text)
+        likeButton = videoSoup.find_all("button", "like-button-renderer-like-button")
+        likeNum = likeButton[0].span.string
+        dislikeButton = videoSoup.find_all("button", "like-button-renderer-dislike-button")
+        dislikeNum = dislikeButton[0].span.string
+        print "Like: "+likeNum+", "+"Dislike: "+dislikeNum+"\n"
+        i+=1
+        if i>=number:
+            break
+
+
+    # get_description(results, number)
     # get_link(results, number)
     # get_title(results, number)
